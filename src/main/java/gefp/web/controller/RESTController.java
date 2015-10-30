7909c107c64bb1c575d8b65ef29c50e60ec2e421
Harsh Gadhia <harshgadhia@gmail.com>
2015-10-30 00:26:14 +0000

    git-svn-id: file:///home/cysun/subversion/gefp/web/trunk@466 1e33301c-9783-4aea-b6a3-3f25a6be623f

--- src/main/java/gefp/web/controller/RESTController.java
+++ src/main/java/gefp/web/controller/RESTController.java
@@ -2,6 +2,8 @@ package gefp.web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,6 +19,10 @@ public class RESTController {
     @Autowired
     UserDao userDao;
 
+    
+    /*
+     * Validate User Login
+     */
     @RequestMapping(value = "/validate", method = RequestMethod.POST)
     @ResponseBody
     public User validateLogin( @RequestParam("username" ) String username,
@@ -26,4 +32,26 @@ public class RESTController {
         return userDao.validateUser( user );
     }
 
+    
+    /*
+     * Fetch a user by his username
+     */
+    @RequestMapping(value = "/fetchuser/{username}", method = RequestMethod.GET)
+    @ResponseBody
+    public User fetchUser( @PathVariable("username" ) String username)
+    {
+        return userDao.getUserByUsername( username );
+    }
+    
+    /*
+     * Update a user profile
+     */
+    @RequestMapping(value = "/updateuser/{username}", method = RequestMethod.PUT)
+    @ResponseBody
+    public User updateUser( @RequestBody User user)
+    {
+        return userDao.saveUser( user );
+    }
+    
+    
 }
