14004118107755e6c89092e1253b25caf1eca638
Harsh Gadhia <harshgadhia@gmail.com>
2016-02-01 07:26:16 +0000

    git-svn-id: file:///home/cysun/subversion/gefp/web/trunk@468 1e33301c-9783-4aea-b6a3-3f25a6be623f

--- src/main/java/gefp/web/service/UserRestController.java
+++ src/main/java/gefp/web/service/UserRestController.java
@@ -4,19 +4,24 @@ import gefp.model.User;
 import gefp.model.dao.UserDao;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class UserRestController {
-    
+
     @Autowired
     UserDao userDao;
-    
+
     @RequestMapping("/api/login")
-    public User greeting(@RequestParam(value="username") String username, @RequestParam(value="password") String password) {
-        return userDao.validateUser( new User(username, password) );
+    public String greeting( @RequestParam(value = "username") String username,
+        @RequestParam(value = "password") String password, ModelMap models )
+    {
+        User user = userDao.validateUser( new User( username, password ) );
+        models.put( "user", user );
+        return "jsonView";
     }
-    
+
 }
