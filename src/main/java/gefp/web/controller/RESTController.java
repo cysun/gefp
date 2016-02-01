f46f0e3641ada7e6a5599c05f72686c3002c6523
Harsh Gadhia <harshgadhia@gmail.com>
2016-02-01 08:01:51 +0000

    git-svn-id: file:///home/cysun/subversion/gefp/web/trunk@475 1e33301c-9783-4aea-b6a3-3f25a6be623f

--- src/main/java/gefp/web/controller/RESTController.java
+++ src/main/java/gefp/web/controller/RESTController.java
@@ -1,57 +1,68 @@
 package gefp.web.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-
 import gefp.model.User;
 import gefp.model.dao.UserDao;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 @Controller
-@RequestMapping("/webapi")
 public class RESTController {
 
+    
     @Autowired
     UserDao userDao;
 
-    
-    /*
-     * Validate User Login
-     */
-    @RequestMapping(value = "/validate", method = RequestMethod.POST)
-    @ResponseBody
-    public User validateLogin( @RequestParam("username" ) String username,
-        @RequestParam("password") String password)
+    @RequestMapping("/api/login")
+    public String login( @RequestParam String username,
+        @RequestParam String password, ModelMap models )
     {
-        User user = new User( username, password );
-        return userDao.validateUser( user );
-    }
-
-    
-    /*
-     * Fetch a user by his username
-     */
-    @RequestMapping(value = "/fetchuser/{username}", method = RequestMethod.GET)
-    @ResponseBody
-    public User fetchUser( @PathVariable("username" ) String username)
-    {
-        return userDao.getUserByUsername( username );
+        User user = userDao.validateUser( new User( username, password ) );
+        models.put( "user", user );
+        return "jsonView";
     }
     
-    /*
-     * Update a user profile
-     */
-    @RequestMapping(value = "/updateuser/{username}", method = RequestMethod.PUT)
-    @ResponseBody
-    public User updateUser( @RequestBody User user)
-    {
-        return userDao.saveUser( user );
-    }
+    
+    
+//    @Autowired
+//    UserDao userDao;
+//
+//    
+//    /*
+//     * Validate User Login
+//     */
+//    @RequestMapping(value = "/validate", method = RequestMethod.POST)
+//    @ResponseBody
+//    public User validateLogin( @RequestParam("username" ) String username,
+//        @RequestParam("password") String password)
+//    {
+//        User user = new User( username, password );
+//        return userDao.validateUser( user );
+//    }
+//
+//    
+//    /*
+//     * Fetch a user by his username
+//     */
+//    @RequestMapping(value = "/fetchuser/{username}", method = RequestMethod.GET)
+//    @ResponseBody
+//    public User fetchUser( @PathVariable("username" ) String username)
+//    {
+//        return userDao.getUserByUsername( username );
+//    }
+//    
+//    /*
+//     * Update a user profile
+//     */
+//    @RequestMapping(value = "/updateuser/{username}", method = RequestMethod.PUT)
+//    @ResponseBody
+//    public User updateUser( @RequestBody User user)
+//    {
+//        return userDao.saveUser( user );
+//    }
     
     
 }
--- src/main/java/gefp/web/service/UserRestController.java
+++ src/main/java/gefp/web/service/UserRestController.java
@@ -1,27 +1,10 @@
 package gefp.web.service;
 
-import gefp.model.User;
-import gefp.model.dao.UserDao;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserRestController {
 
-    @Autowired
-    UserDao userDao;
-
-    @RequestMapping("/api/login")
-    public String login( @RequestParam String username,
-        @RequestParam String password, ModelMap models )
-    {
-        User user = userDao.validateUser( new User( username, password ) );
-        models.put( "user", user );
-        return "403";
-    }
+    
 
 }
