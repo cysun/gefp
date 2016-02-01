ee414d68d14f8eaa0665ce88c134223c73aa82d4
Harsh Gadhia <harshgadhia@gmail.com>
2016-02-01 08:10:28 +0000

    git-svn-id: file:///home/cysun/subversion/gefp/web/trunk@476 1e33301c-9783-4aea-b6a3-3f25a6be623f

--- src/main/java/gefp/web/controller/RESTController.java
+++ src/main/java/gefp/web/controller/RESTController.java
@@ -1,31 +1,10 @@
 package gefp.web.controller;
 
-import gefp.model.User;
-import gefp.model.dao.UserDao;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class RESTController {
-
-    
-    @Autowired
-    UserDao userDao;
-
-    @RequestMapping("/api/login")
-    public String login( @RequestParam String username,
-        @RequestParam String password, ModelMap models )
-    {
-        User user = userDao.validateUser( new User( username, password ) );
-        models.put( "user", user );
-        return "jsonView";
-    }
-    
-    
+   
     
 //    @Autowired
 //    UserDao userDao;
--- src/main/java/gefp/web/service/UserRestController.java
+++ src/main/java/gefp/web/service/UserRestController.java
@@ -1,10 +1,30 @@
 package gefp.web.service;
 
+import gefp.model.User;
+import gefp.model.dao.UserDao;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("/api/login")
 public class UserRestController {
 
-    
+    @Autowired
+    UserDao userDao;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public @ResponseBody User login( @RequestParam String username,
+        @RequestParam String password, ModelMap models )
+    {
+        User user = userDao.validateUser( new User( username, password ) );
+        //models.put( "user", user );
+        return user;
+    }
 
 }
