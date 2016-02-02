b49c859501242b79a87eedc152dc413194908eec
Harsh Gadhia <harshgadhia@gmail.com>
2016-02-02 06:21:49 +0000

    git-svn-id: file:///home/cysun/subversion/gefp/web/trunk@489 1e33301c-9783-4aea-b6a3-3f25a6be623f

--- src/main/java/gefp/web/service/UserRestService.java
+++ src/main/java/gefp/web/service/UserRestService.java
@@ -3,6 +3,8 @@ package gefp.web.service;
 import gefp.model.User;
 import gefp.model.dao.UserDao;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -14,11 +16,15 @@ public class UserRestService {
 
     @Autowired
     UserDao userDao;
-
+    
+    private static final Logger logger = LoggerFactory.getLogger( UserRestService.class );
+    
     @RequestMapping("/api/login")
     public String login( @RequestParam String username,
         @RequestParam String password, ModelMap models )
     {
+        logger.info( "Login API called.." );
+        
         User user = userDao.validateUser( new User( username, password ) );
         models.put( "user", user );
         return "jsonView";
