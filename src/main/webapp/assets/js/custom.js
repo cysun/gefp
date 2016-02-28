9d6c9a9a812398b431617aa929092f796c1832bb
Harsh Gadhia <harshgadhia@gmail.com>
2016-02-28 22:22:41 +0000

    git-svn-id: file:///home/cysun/subversion/gefp/web/trunk@609 1e33301c-9783-4aea-b6a3-3f25a6be623f

--- src/main/webapp/assets/js/custom.js
+++ src/main/webapp/assets/js/custom.js
@@ -135,6 +135,7 @@ function ShowStudentsPlan(studentid) {
 					data : {
 						message : "",
 						userId : $(curr).attr("data-userId"),
+						accessKey : $("#accessKey").val(),
 						id : $(curr).val(),
 						checked : checked
 					},
